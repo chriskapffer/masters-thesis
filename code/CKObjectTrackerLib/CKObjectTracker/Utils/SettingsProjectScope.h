fb135131bb00b0eaaf9e6c3246305f9e2147c959
codingChris <coding@chris.com>
2012-09-05 00:41:42 +0200

    improves template structure and project scope

---
+++ code/CKObjectTrackerLib/CKObjectTracker/Utils/SettingsProjectScope.h
@@ -0,0 +1,14 @@
+//
+//  SettingsProjectScope.h
+//  CKObjectTrackerLib
+//
+//  Created by Christoph Kapffer on 05.09.12.
+//  Copyright (c) 2012 HTW Berlin. All rights reserved.
+//
+
+#ifndef CKObjectTrackerLib_SettingsProjectScope_h
+#define CKObjectTrackerLib_SettingsProjectScope_h
+
+
+
+#endif
