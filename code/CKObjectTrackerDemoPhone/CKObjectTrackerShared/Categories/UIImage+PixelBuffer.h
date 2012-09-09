e7909b451fbe4e44cda096090c867867dc6d59cb
codingChris <coding@chris.com>
2012-09-09 18:34:14 +0200

    fixes bugs, adds debug views to phone apps and adds settings to phone demo

---
+++ code/CKObjectTrackerDemoPhone/CKObjectTrackerShared/Categories/UIImage+PixelBuffer.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+PixelBuffer.h
+//  CKObjectTrackerDemoPhone
+//
+//  Created by Christoph Kapffer on 09.09.12.
+//  Copyright (c) 2012 HTW Berlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (PixelBuffer)
+
+@end
---
+++ code/CKObjectTrackerDemoPhone/CKObjectTrackerShared/Categories/UIImage+PixelBuffer.m
@@ -0,0 +1,13 @@
+//
+//  UIImage+PixelBuffer.m
+//  CKObjectTrackerDemoPhone
+//
+//  Created by Christoph Kapffer on 09.09.12.
+//  Copyright (c) 2012 HTW Berlin. All rights reserved.
+//
+
+#import "UIImage+PixelBuffer.h"
+
+@implementation UIImage (PixelBuffer)
+
+@end
